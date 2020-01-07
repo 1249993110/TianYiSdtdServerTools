ecae6c88b339963a1b51175939c7614e4c9554b2
IceCoffee <1249993110@qq.com>
2020-01-07 14:58:46 +0800

    添加项目文件。

---
+++ Client/Client/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TianYiSdtdServerTools.Client
+{
+    public class Startup
+    {
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
+        public static void Main()
+        {
+            App app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
---
+++ Shared/Shared/UserGroups/UserGroup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TianYiSdtdServerTools.Shared.UserGroups
+{
+    /// <summary>
+    /// 用户群组
+    /// </summary>
+    public enum UserGroup : int
+    {
+        /// <summary>
+        /// 黑名单用户
+        /// </summary>
+        BlacklistUser = -1,
+
+        /// <summary>
+        /// 普通用户
+        /// </summary>
+        NormalUser = 0,
+        
+        /// <summary>
+        /// 普通会员
+        /// </summary>
+        Vip,
+
+        /// <summary>
+        /// 优惠会员
+        /// </summary>
+        ConcessionVip,
+
+        /// <summary>
+        /// 管理员
+        /// </summary>
+        Administrator,
+
+        /// <summary>
+        /// 超级管理员
+        /// </summary>
+        SuperAdministrator
+    }
+}
